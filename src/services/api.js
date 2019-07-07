192ef152ee8dbb2e742df3e3f6f75de08fb732b2
André Coelho <avrcoelho@gmail.com>
2019-07-07 12:57:02 -0300

    ajustes na conexao com a API

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { isAuthenticated } from './auth';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'http://localhost:3000',
 });
 
 api.interceptors.request.use(async (config) => {
